d83078dfcbf8c16796758e3d09c88686a84a63bb
Kerwyn Rojas <kerwyn.rg@gmail.com>
2015-12-29 16:23:44 -0700

    Added semicolons in .eslintrc and fixed runned

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import { createHistory as createBrowserHistory } from 'history'
-import { syncReduxAndRouter } from 'redux-simple-router'
-import routes from './routes'
-import Root from './containers/Root'
-import configureStore from './redux/configureStore'
+import { createHistory as createBrowserHistory } from 'history';
+import { syncReduxAndRouter } from 'redux-simple-router';
+import routes from './routes';
+import Root from './containers/Root';
+import configureStore from './redux/configureStore';
 
 // ----- Can go away when react 1.0 release -------------
-import injectTapEventPlugin from 'react-tap-event-plugin'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 // Can go away when react 1.0 release
 // Check this repo:
 // https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin()
+injectTapEventPlugin();
 // ------------------------------------------------------
 
-const history = createBrowserHistory()
-const store = configureStore(window.__INITIAL_STATE__)
+const history = createBrowserHistory();
+const store = configureStore(window.__INITIAL_STATE__);
 
-syncReduxAndRouter(history, store, (state) => state.router)
+syncReduxAndRouter(history, store, (state) => state.router);
 
 // Render the React application to the DOM
 ReactDOM.render(
   <Root history={history} routes={routes} store={store} />,
   document.getElementById('root')
-)
+);
