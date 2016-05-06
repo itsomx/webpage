fc8385ad307051896a8013792803552778f36b0c
Kerwyn Rojas <kerwyn.rg@gmail.com>
2016-05-06 15:35:36 -0600

    Parallax removed

--- src/views/MainView.jsx
+++ src/views/MainView.jsx
@@ -1,19 +1,37 @@
-import { Parallax } from 'react-parallax';
+import Section from 'components/Section';
 import MainBar from 'components/MainBar';
 import MainBackground from 'components/MainBackground';
 
 import backgroundImage from 'static/images/main-background.jpg';
 
 export default class MainView extends React.Component {
+
+  static propTypes = {
+    style: React.PropTypes.object
+  }
+
+  static defaultProps = {
+    style: {}
+  }
+
   render = () => {
     let {
+      style,
       ...other
     } = this.props;
+
+    style = Object.assign({}, {
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center'
+    }, style);
+
     return (
-      <Parallax bgImage={backgroundImage} strength={400} blur={2} {...other}>
+      <Section style={style} className='here' {...other}>
         <MainBar />
         <MainBackground />
-      </Parallax>
+      </Section>
     );
   }
 }
