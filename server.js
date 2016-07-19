9643cb20ed8274ec2cde30f92b60940b4f87de9b
Kerwyn Rojas <kerwyn.rg@gmail.com>
2016-07-19 16:15:14 -0600

    Fixed node port

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ var server = {
     var indexPath = path.join(__dirname, '/dist/index.html');
     var publicPath = express.static(path.join(__dirname, '/dist'));
 
+    app.set('port', (process.env.PORT || 5000));
     app.use('/', publicPath);
     app.get('/', function (_, res) {
       res.sendFile(indexPath);
     });
 
-    app.listen(3033);
+    app.listen(app.get('port'), function () {
+      console.log('Node app is running on port', app.get('port'));
+    });
 
     return server;
   }
